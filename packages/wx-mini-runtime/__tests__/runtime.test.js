59ce82f8b45ac36a9d1c11adbbaab482ff38c56d
yiper <yiper.fan@gmail.com>
2022-02-20 23:09:54 +0800

    up

--- packages/wx-mini-runtime/__tests__/runtime.test.js
+++ packages/wx-mini-runtime/__tests__/runtime.test.js
@@ -3,9 +3,13 @@ import { pp, ppRef } from '../index.js';
 test('pp proxy is ok', () => {
     const data = pp({ a: 1 });
     expect(data.a).toBe(1);
+    data.a = { b: 2 };
+    expect(data.a.b).toBe(2);
 });
 
 test('ppRef proxy is ok', () => {
     const data = ppRef(1);
     expect(data.value).toBe(1);
+    data.value = { b: 2 };
+    expect(data.value.b).toBe(2);
 });
